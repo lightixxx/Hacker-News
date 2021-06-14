033dd03e84d815652e703a2a87f77b952869d0c2
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-06-14 18:22:55 +0900

    ♻️ Remove duplicate code

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const content = document.createElement('div')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
-ajax.open('GET', NEWS_URL, false)
-ajax.send()
+const getData = (url) => {
+  ajax.open('GET', url, false)
+  ajax.send()
 
-const newsFeed = JSON.parse(ajax.response)
+  return JSON.parse(ajax.response)
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul')
 
 const hashchangeHandler = () => {
   const id = location.hash.substr(1)
 
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false)
-  ajax.send()
-
-  const newsContent = JSON.parse(ajax.response)
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
   const title = document.createElement('h1')
 
   title.innerHTML = newsContent.title
@@ -27,14 +28,17 @@ const hashchangeHandler = () => {
 window.addEventListener('hashchange', hashchangeHandler)
 
 for(let i = 0; i < newsFeed.length; i++) {
-  const li = document.createElement('li')
-  const a = document.createElement('a')
-  
-  a.href = `#${newsFeed[i].id}`
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`
+  const div = document.createElement('div')
 
-  li.appendChild(a)
-  ul.appendChild(li)
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `
+
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul)
