82c4e921dd8bd2006e4407acd5e22a38b1d027c0
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-06-15 18:23:40 +0900

    ✨ Use a router

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const ajax = new XMLHttpRequest()
 const content = document.createElement('div')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const store = {
+  currentPage: 1,
+  lastPage: Math.floor(NEWS_URL.length / 10),
+}
 
 const getData = (url) => {
   ajax.open('GET', url, false)
@@ -17,32 +21,35 @@ const getNewsFeed = () => {
 
   newsList.push('<ul>')
 
-  for(let i = 0; i < newsFeed.length; i++) {
-    const div = document.createElement('div')
-
-  newsList.push( `
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `)
-}
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push( `
+      <li>
+        <a href="#/show/${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `)
+  }
 
 newsList.push('</ul>')
-
+newsList.push(`
+  <div>
+    <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전</a>
+    <a href="#/page/${store.currentPage < store.lastPage ? store.currentPage + 1 : store.lastPage}">다음</a>
+  </div>
+`)
 container.innerHTML = newsList.join('')
 }
 
 const newsDetail = () => {
-  const id = location.hash.substr(1)
+  const id = location.hash.substr(7)
   const newsContent = getData(CONTENT_URL.replace('@id', id))
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#"> 목록으로 </a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `
 }
@@ -50,7 +57,10 @@ const newsDetail = () => {
 const router = () => {
   const routePath = location.hash
 
-  if(routePath === '') {
+  if (routePath === '') {
+    getNewsFeed()
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7))
     getNewsFeed()
   } else {
     newsDetail()
